6684802b88dc32e9e0253ee40c2ce2a20d52e19b
chakri8556 <chakri8556@gmail.com>
2021-12-14 19:31:26 +0530

    TenthCommit

---
+++ user_Dealer/src/main/java/user_Dealer/Services/CustomUserDetailsService.java
@@ -0,0 +1,21 @@
+package user_Dealer.Services;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        if(username.equals("Kumar")){
+            return new User("Kumar","Kumar234",new ArrayList<>());
+        }else{
+            throw new UsernameNotFoundException("User not found!!");
+        }
+    }
+}
--- user_Farmer/src/main/java/user_Farmer/UserFarmerApplication.java
+++ user_Farmer/src/main/java/user_Farmer/UserFarmerApplication.java
@@ -5,8 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import user_Farmer.model.Farmer;
+
+import java.util.List;
+import java.util.stream.Stream;
+
 @SpringBootApplication
-public class UserFarmerApplication {
+public class UserFarmerApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserFarmerApplication.class, args);
